5ef586dc61056036e0e42c58b75573b430a3be32
priyankashetty26 <priyankashetty2603third@gmail.com>
2025-07-09 17:47:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "ec2" {
   source        = "./modules/ec2"
-  ami           = "ami-004c0b94e90d754e9"
+  ami           = "ami-00c8ac9147e19828e"
   #In regions like Stockholm (eu-north-1), t2.micro isn't available, and the free tier includes t3.micro.
   instance_type = "t3.micro"
   key_name      = "testkey"
